a7223ef678ae08a329ed6e106810ffa7b0f591a6
bumbrt <bumbrt@gmail.com>
2023-01-23 02:18:22 +0400

    mega parser

--- src/HtmlComments.ts
+++ src/HtmlComments.ts
@@ -21,11 +21,10 @@ export class HtmlCommentWithTags {
 }
 
 export class HtmlCommentTag {
-    readonly fullName: string
+    // readonly fullName: string
     readonly name: string
     readonly child: HtmlCommentTag | null
     constructor(tagString: string) {
-        this.fullName = tagString;
         const index = tagString.indexOf("/");
         if (index > 0) {
             this.name = tagString.substring(0, index);
@@ -39,36 +38,6 @@ export class HtmlCommentTag {
             this.name = tagString;
             this.child = null;
         }
-
-    }
-}
-
-/**
- * Organise collection of comments with tags to structure for tree:
- [
-    {
-        label: "test",
-        key: "item-0",
-        type: "tag",
-        children: [
-            {
-                label: "child test1",
-                key: "item-1",
-                type: "tag",
-                children: [
-                    label: "Comment body",
-                    key: "comment-id",
-                    line: 112,
-                    type: "comment",
-                ]
-            }
-        ]
-    }
-];
- */
-export class OrganasiedByTagHtmlComments {
-
-    constructor(commentsWithTags: HtmlCommentWithTags[]) {
 
     }
 
