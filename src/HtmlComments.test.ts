df1d37a94803f1385e1bdbe2803d64531f3de376
bumbrt <bumbrt@gmail.com>
2023-01-23 22:53:02 +0400

    ebaniy styd

--- src/HtmlComments.test.ts
+++ src/HtmlComments.test.ts
@@ -1,6 +1,15 @@
-import {describe, expect, test} from '@jest/globals';
+import { describe, expect, test } from '@jest/globals';
 import { HtmlCommentWithTags, HtmlCommentTag } from './HtmlComments'
 
+test("should organise comments to tree view", () => {
+    const textToAnalyse = `
+    Rule* ( Common, One, Two, Three, Four, Practice, Theory, Hud) - описание правила чтения
+Struct - структура книги (кратко методы и способы чтения)
+<span class="ob-html-comment" id="comment-92d3475d-b262-4a8b-8990-b67f182fb4c1" data-tags="[comment,#test]"><span class="ob-html-comment-body">#test CommentPlaceholder</span>Tip - **практический** совет</span>
+<span class="ob-html-comment" id="comment-e5e2a999-de9b-4582-ab4d-f7b666a89bd0" data-tags="[comment,]"><span class="ob-html-comment-body">CommentPlaceholder</span>Class - классификация книг, наук</span>
+    `;
+
+});
 
 test.skip('should match regex', () => {
     const regExSpan =
@@ -14,36 +23,40 @@ test.skip('should match regex', () => {
     console.log(result);
 });
 
+describe("parsing tag", () => {
+    test.skip('should parse simple tag', () => {
+        const tagStr = "comments";
+        const parsedTag = new HtmlCommentTag(tagStr);
+        expect(parsedTag.name).toBe("comments");
+    });
 
-test.skip('should parse simple tag', () => {
-    const tagStr = "comments";
-    const parsedTag = new HtmlCommentTag(tagStr);
-    expect(parsedTag.name).toBe("comments");
+    test.skip('should parse simple hierarchical tag', () => {
+        const tagStr = "parent/child/child-two";
+        const parsedTag = new HtmlCommentTag(tagStr);
+        expect(parsedTag.name).toBe("parent");
+        expect(parsedTag.child?.name).toBe("child");
+        expect(parsedTag.child?.child?.name).toBe("child-two");
+    });
 });
 
-test.skip('should parse simple hierarchical tag', () => {
-    const tagStr = "parent/child/child-two";
-    const parsedTag = new HtmlCommentTag(tagStr);
-    expect(parsedTag.name).toBe("parent");
-    expect(parsedTag.child?.name).toBe("child");
-    expect(parsedTag.child?.child?.name).toBe("child-two");
-});
+describe("parsing multiple tags from coma separated string", () => {
+    test.skip('should parse tags string', () => {
+        const tagsStr = "comments,tag";
+        const parsed = new HtmlCommentWithTags("", tagsStr, "", 0);
+        expect(parsed.tags.length).toBe(2);
+    });
 
-test.skip('should parse tags string', () => {
-    const tagsStr = "comments,tag";
-    const parsed = new HtmlCommentWithTags("", tagsStr, "", 0);
-    expect(parsed.tags.length).toBe(2);
-});
-
-test.skip('should parse tags string, ignore coma', () => {
-    const tagsStr = "comments,";
-    const parsed = new HtmlCommentWithTags("", tagsStr, "", 0);
-    expect(parsed.tags.length).toBe(1);
-});
+    test.skip('should parse tags string, ignore coma', () => {
+        const tagsStr = "comments,";
+        const parsed = new HtmlCommentWithTags("", tagsStr, "", 0);
+        expect(parsed.tags.length).toBe(1);
+    });
 
 
-test.skip('should parse tags string with duplicate', () => {
-    const tagsStr = "comments,tag,tag";
-    const parsed = new HtmlCommentWithTags("", tagsStr, "", 0);
-    expect(parsed.tags.length).toBe(2);
+    test.skip('should parse tags string with duplicate', () => {
+        const tagsStr = "comments,tag,tag";
+        const parsed = new HtmlCommentWithTags("", tagsStr, "", 0);
+        expect(parsed.tags.length).toBe(2);
+    });
 });
+
--- src/HtmlComments.ts
+++ src/HtmlComments.ts
@@ -1,3 +1,4 @@
+import { TreeOption } from 'naive-ui';
 
 export class HtmlCommentWithTags {
     readonly id: string
@@ -16,29 +17,151 @@ export class HtmlCommentWithTags {
         );
         this.tagsNames = new Set(tagsArr);
 
-        this.tags = [...this.tagsNames].map(tag => new HtmlCommentTag(tag));
+        this.tags = [...this.tagsNames].map(tag => new HtmlCommentTag(tag, null, null));
     }
 }
 
 export class HtmlCommentTag {
     // readonly fullName: string
     readonly name: string
+    readonly treeId: string
+    readonly treeLevel: number
     readonly child: HtmlCommentTag | null
-    constructor(tagString: string) {
+    constructor(tagString: string, parentTreeKey: string | null = null, parentTreeLevel: number | null = null) {
+        // this.fullName = tagString;
+        if (parentTreeLevel == null) {
+            this.treeLevel = 0;
+        } else {
+            this.treeLevel = parentTreeLevel + 1;
+        }
+        if (this.treeLevel > 3) {
+            throw Error("Only three nested tags supported, test/test1/test2/test3")
+        }
         const index = tagString.indexOf("/");
         if (index > 0) {
             this.name = tagString.substring(0, index);
+            this.treeId = `${parentTreeKey}/${this.name}`;
             const childTagString = tagString.substring(index + 1);
             if (childTagString) {
-                this.child = new HtmlCommentTag(childTagString);
+                this.child = new HtmlCommentTag(childTagString, this.treeId, this.treeLevel);
             } else {
                 this.child = null;
             }
         } else {
+            this.treeId = `${parentTreeKey}/${this.name}`;
             this.name = tagString;
             this.child = null;
         }
 
+    }
+}
+
+/**
+ * Organise collection of comments with tags to structure for tree:
+ [
+    {
+        label: "test",
+        key: "item-0",
+        type: "tag",
+        children: [
+            {
+                label: "child test1",
+                key: "item-1",
+                type: "tag",
+                children: [
+                    label: "Comment body",
+                    key: "comment-id",
+                    line: 112,
+                    type: "comment",
+                ]
+            }
+        ]
+    }
+];
+ */
+
+interface TreeOptionWithChild extends TreeOption {
+    children: TreeOption[]
+}
+
+export class HtmlCommentsOrganasiedToViewTree {
+    readonly treeOptions: TreeOption[]
+    readonly commentsWithTags: HtmlCommentWithTags[]
+
+    constructor(commentsWithTags: HtmlCommentWithTags[]) {
+        this.commentsWithTags = commentsWithTags
+        this.treeOptions = []
+        const rootTags = commentsWithTags.filter(
+            it => it.tags
+        ).flatMap(
+            it => it.tags
+        );
+        const rootTagsByName = this.groupTagsByName(rootTags);
+        this.processGroupedTags(this.treeOptions, rootTagsByName);
+    }
+
+    processGroupedTags(currentTreeLevel: TreeOption[], groupedTags: Map<string, HtmlCommentTag[]>) {
+        groupedTags.forEach(
+            (optionTags, key) => {
+                const currentOption = this.tagToTreeOption(key, key);
+                this.treeOptions.push(currentOption);
+                const comments = this.commentsWithTags.filter(
+                    it => it.tags.some(
+                        tag => optionTags.some(
+                            ot => ot.treeId === tag.treeId
+                        )
+                    )
+                );
+                const childTags = optionTags.map(
+                    it => it.child
+                ).filter((it): it is HtmlCommentTag => it != null);
+                const childTagsByName = this.groupTagsByName(childTags);
+                this.processGroupedTags(currentOption.children, childTagsByName)
+                currentOption.children?.push(...this.commentsWithoutTagsToTreeOptions(comments))
+            }
+        )
+
+        currentTreeLevel.push(...this.commentsWithoutTagsToTreeOptions(this.commentsWithTags));
+    }
+
+    commentsWithoutTagsToTreeOptions(comments: HtmlCommentWithTags[]): TreeOption[] {
+        return comments.filter(
+            it => !it.tags
+        ).map (
+            it => this.commentToTreeOption(it)
+        )
+    }
+    commentToTreeOption(comment: HtmlCommentWithTags): TreeOption {
+        return <TreeOption>{
+            type: "comment",
+            key: comment.id,
+            label: comment.commentBody,
+            line: comment.line,
+        }
+    }
+
+    tagToTreeOption(key: string, name: string): TreeOptionWithChild {
+        return <TreeOptionWithChild>{
+            type: "tag",
+            key: key,
+            label: name,
+            children: []
+        }
+    }
+
+    groupTagsByName(tags: Array<HtmlCommentTag>): Map<string, HtmlCommentTag[]> {
+        const tagsByName = new Map<string, HtmlCommentTag[]>;
+        tags.forEach(
+            tag => {
+                let tags = tagsByName.get(tag.name);
+                if (tags == null) {
+                    tags = [];
+                    tagsByName.set(tag.name, tags);
+                }
+                tags.push(tag);
+            }
+        );
+        return tagsByName;
     }
 
 }
